fbc5949ca5a5425229e0fe7b3e2cf2d0429c845b
Misha S. <misha.sytch@mail.ru>
2024-04-15 12:56:55 +0300

    Создан класс для работы с файлами FileViewer

--- app/src/main/java/hex/editor/services/FileViewer.java
+++ app/src/main/java/hex/editor/services/FileViewer.java
@@ -1,21 +1,30 @@
 package hex.editor.services;
 
-import java.io.File;
-import java.util.Scanner;
+import java.io.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.util.*;
 
 public class FileViewer {
-    private File file;
-    private Scanner scanner;
-    private String path;
+    private Path path;
 
 
     public FileViewer(String path) {
-        this.path = path;
-        file = new File(path);
+        this.path = Paths.get(path);
     }
 
-    public String getLine(int from, int to) {
-        
+    public List<String> getLine(int from, int to) {
+        List<String> lines = new ArrayList<String>();
+        try (Stream<String> stream = Files.lines(path)) {
+            lines = stream.toList();
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        return lines;
     }
 
 
