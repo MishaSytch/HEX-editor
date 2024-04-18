d9b9b4e250eaef2e3c577a421b7757900896c178
Misha S <misha.sytch@mail.ru>
2024-04-18 19:13:44 +0300

    HEXservice больше не зависит от конкретного файла, т.е. переводит конкретные String; Не все методы реализованны, а именно перевод из Chars в Hex и обратно

--- app/src/main/java/hex/editor/services/HEXservice.java
+++ app/src/main/java/hex/editor/services/HEXservice.java
@@ -1,18 +1,10 @@
 package hex.editor.services;
 
-import java.util.stream.Collectors;
 import java.util.Arrays;
 
 public class HEXservice {
-    private FileViewer fileViewer;
-    private String lines;  
 
-    public void readLinesFromFile(String path) {
-        fileViewer = new FileViewer(path);
-        lines = fileViewer.getLines().stream().collect(Collectors.joining(""));
-    }
-
-    public String[] getHex() {
+    public String[] getHexFromString(String lines) {
         return Arrays.stream(lines.split(""))
             .map(s -> s.charAt(0))
             .map(c -> {
@@ -22,13 +14,24 @@ public class HEXservice {
             .toArray(String[]::new);
     }
 
-    public Byte[] getBytes() {
-        return Arrays.stream(getHex())
-            .map(x -> x.getBytes())
-            .toArray(Byte[]::new);
+    public String[] getCharsFromString(String lines) {
+        return lines.split("");
     }
 
-    public String[] getChars() {
-        return lines.split("");
+    public String[] getCharsFromHex(String[] hex) {
+        return Arrays.stream(hex)
+            .map(h -> Integer.valueOf(h, 10))
+            .toArray(String[]::new);
+            
+    }
+
+    public String[] getHexFromChars(String[] chars) {
+        return Arrays.stream(chars)
+            .map(s -> s.charAt(0))
+            .map(c -> {
+                if((int)c > 256) return String.format("%04x", (int)c).toUpperCase();
+                return String.format("%02x", (int)c).toUpperCase();
+            })
+            .toArray(String[]::new);
     }
 }
