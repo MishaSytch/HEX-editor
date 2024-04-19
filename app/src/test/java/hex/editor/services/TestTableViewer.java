7a94a4a3e1a13d50e442133c94726b0e4be8aa3e
Misha S. <misha.sytch@mail.ru>
2024-04-19 21:07:51 +0300

    Создан класс для представления таблиц данных

--- app/src/test/java/hex/editor/services/TestTableViewer.java
+++ app/src/test/java/hex/editor/services/TestTableViewer.java
@@ -5,7 +5,7 @@ import org.junit.gen5.api.Test;
 
 public class TestTableViewer {
     @Test
-    void getTable() {
+    void testGetTable_Chars() {
         TableViewer tableViewer = new TableViewer();
 
         String[][] tb = tableViewer.getTable("Pulvinar ".split(""), 3);
@@ -13,6 +13,20 @@ public class TestTableViewer {
             new String[]{"P", "u", "l"},
             new String[]{"v", "i", "n"},
             new String[]{"a", "r", " "}
+        };
+        
+        Assertions.assertEquals(tb, res);
+    }
+
+    @Test
+    void testGetTable_Hex() {
+        TableViewer tableViewer = new TableViewer();
+
+        String[][] tb = tableViewer.getTable(HexService.getHexFromString("Pulvinar "), 3);
+        String[][] res = new String[][] {
+            new String[]{"50", "75", "6C"},
+            new String[]{"76", "69", "6E"},
+            new String[]{"61", "72", "20"}
         };
         
         Assertions.assertEquals(tb, res);
