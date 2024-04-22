6bff696a2f8d83706417431dd3ed8ea9fb55ecb0
Misha S. <misha.sytch@mail.ru>
2024-04-23 00:33:18 +0300

    Добавлен тест

--- app/src/test/java/hex/editor/services/TestTableViewer.java
+++ app/src/test/java/hex/editor/services/TestTableViewer.java
@@ -1,5 +1,7 @@
 package hex.editor.services;
 
+import javax.swing.table.TableModel;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -18,8 +20,8 @@ public class TestTableViewer {
 
     @Test
     void testGetTable_Hex() {
-        String[][] tb = TableViewer.getTable(HexService.getHexFromString("Pulvinar "), 3);
-        String[][] res = new String[][] {
+        TableModel tb = TableViewer.getTable(HexService.getHexFromString("Pulvinar "), 3);
+        TableModel res = new  {
             new String[]{"0", "50", "75", "6C"},
             new String[]{"1", "76", "69", "6E"},
             new String[]{"2", "61", "72", "20"}
