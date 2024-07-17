8dec38e8789046cd74518b5b17bdea7139c399c6
Misha S. <misha.sytch@mail.ru>
2024-07-17 14:24:19 +0300

    Универсальный сепаратор

--- app/src/test/java/hex/editor/services/FilePaths.java
+++ app/src/test/java/hex/editor/services/FilePaths.java
@@ -1,6 +1,7 @@
 package hex.editor.services;
 
 import java.io.File;
+import java.nio.file.FileSystems;
 
 public class FilePaths {
     private static final String textFile = "app/src/test/resources/text.txt";
@@ -8,11 +9,13 @@ public class FilePaths {
 
     public static String getSaveFile() {
         File resourcesDirectory = new File(saveFile);
-        return resourcesDirectory.getAbsolutePath().replace("/app/app/", "/app/");
+        return resourcesDirectory.getAbsolutePath()
+                .replace("app" + System.getProperty("file.separator") + "app", "app");
     }
 
     public static String getTextFile() {
         File resourcesDirectory = new File(textFile);
-        return resourcesDirectory.getAbsolutePath().replace("/app/app/", "/app/");
+        return resourcesDirectory.getAbsolutePath()
+                .replace("app" + System.getProperty("file.separator") + "app", "app");
     }
 }
